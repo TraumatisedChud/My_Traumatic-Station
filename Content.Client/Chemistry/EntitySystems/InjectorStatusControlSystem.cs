space-wizards/space-station-14

Cleanup: ProtoMan Part 2 - remove Client and Server EntitySystem IPrototypeManager instances (#44416)
* Remove all IPrototypeManager instances

* Remove redundant IComponentFactory instances too

* Found some stragglers

* DestructibleSystem.PrototypeManager: no dependency

* not based

Content.Client/Chemistry/EntitySystems/InjectorStatusControlSystem.cs
@@ -2,19 +2,16 @@
 using Content.Client.Items;
 using Content.Shared.Chemistry.Components;
 using Content.Shared.Chemistry.EntitySystems;
-using Robust.Shared.Prototypes;
 
 namespace Content.Client.Chemistry.EntitySystems;
 
 public sealed partial class InjectorStatusControlSystem : EntitySystem
 {
     [Dependency] private SharedSolutionContainerSystem _solutionContainers = default!;
-    [Dependency] private IPrototypeManager _prototypeManager = default!;
-
 
     public override void Initialize()
     {
         base.Initialize();
-        Subs.ItemStatus<InjectorComponent>(injector => new InjectorStatusControl(injector, _solutionContainers, _prototypeManager));
+        Subs.ItemStatus<InjectorComponent>(injector => new InjectorStatusControl(injector, _solutionContainers, ProtoMan));
     }
 }

TraumatisedChud/My_Traumatic-Station

Content.Client/Chemistry/EntitySystems/InjectorStatusControlSystem.cs
@@ -2,20 +2,17 @@ using Content.Client.Chemistry.UI;
 using Content.Client.Items;
 using Content.Shared.Chemistry.Components;
 using Content.Shared.Chemistry.EntitySystems;
-using Robust.Shared.Prototypes;
 
 namespace Content.Client.Chemistry.EntitySystems;
 
 public sealed partial class InjectorStatusControlSystem : EntitySystem
 {
     [Dependency] private InjectorSystem _injector = default!; // Trauma - replaced _solutionContainers with _injector
-    [Dependency] private IPrototypeManager _prototypeManager = default!;
-
 
     public override void Initialize()
     {
         base.Initialize();
         // Trauma - replace _solutionContainers with _injector
-        Subs.ItemStatus<InjectorComponent>(injector => new InjectorStatusControl(injector, _injector, _prototypeManager));
+        Subs.ItemStatus<InjectorComponent>(injector => new InjectorStatusControl(injector, _injector, ProtoMan));
     }
 }